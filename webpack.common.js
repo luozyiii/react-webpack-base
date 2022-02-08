2e1885dd391ec07ff2d100bb0cbe5372b4686489
罗智毅 <zhiyiluo@drgou.com>
2022-02-08 10:12:53 +0800

    clean-webpack-plugin

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const SimpleProgressWebpackPlugin = require('simple-progress-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const resolve = (dir) => path.resolve(__dirname, dir);
 
@@ -33,12 +34,15 @@ module.exports = {
   },
   target: ['web', 'es5'],
   plugins: [
+    // 清除dist文件夹
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: './public/index.html'
     }),
     new SimpleProgressWebpackPlugin()
   ],
   output: {
+    path: path.resolve(process.cwd(), 'dist'),
     filename: '[name]-[chunkhash].js'
   }
 };
